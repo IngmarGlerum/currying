3b895848d765a92fc94b60a2841f80599373448d
Ingmar Glerum <ingmar.glerum@kvk.nl>
2025-02-28 15:03:29 +0100

    tot 20% van 1e cursus

---
+++ ingmar-next-app/app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import AddToCart from "./AddToCart";
+
+const ProductCard = () => {
+  return (
+    <>
+      <div>ProductCard</div>
+      <AddToCart />
+    </>
+  );
+};
+
+export default ProductCard;
